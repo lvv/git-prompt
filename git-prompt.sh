2accfceef451d50e3a16334c51bf732302a5f5cd
Leonid Volnitsky <Leonid@Volnitsky.com>
2011-05-17 08:29:31 +0300

    -- commit test

--- git-prompt.sh
+++ git-prompt.sh
@@ -317,7 +317,6 @@ set_shell_label() {
         # we might already have short host name
         host=${host%.$default_domain}
 
-
 #################################################################### WHO_WHERE
         #  [[user@]host[-tty]]
 
