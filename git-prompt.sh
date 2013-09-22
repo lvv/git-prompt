ee4c52d26c5b40d2c0b176c9abe8da22937d348f
Juhász Péter <juhasz.peter@osski.hu>
2013-09-22 10:56:56 +0200

    -Fix: backslash in id needs to be escaped if colorized

--- git-prompt.sh
+++ git-prompt.sh
@@ -388,7 +388,7 @@ set_shell_label() {
 
         if [[ -n $id  || -n $host ]] ;   then
                 [[ -n $id  &&  -n $host ]]  &&  at='@'  || at=''
-                color_who_where="${id}${host:+$at_color$at$host_color$host}${tty:+ $tty}"
+                color_who_where="${id//\\/\\\\}${host:+$at_color$at$host_color$host}${tty:+ $tty}"
                 plain_who_where="${id}$at$host"
 
                 # add trailing " "
